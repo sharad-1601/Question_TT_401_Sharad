cf4bd6ecd66d23ff257547a5a06c52abf1449b63
sharad-1601 <112608959+sharad-1601@users.noreply.github.com>
2023-04-13 23:17:34 +0530

    Add files via upload

---
+++ next smaller to left.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+using namespace std;
+
+vector<int> nextsmallertoleft(vector<int>arr){ 
+	vector<int>ans;
+	stack<int>st;
+	for(int i=0;i<arr.size();i++){
+		if(st.size()==0){
+			ans.push_back(-1);
+		}
+		else if(st.size()>0 && st.top()<arr[i]){
+			ans.push_back(st.top());
+		}
+		else if(st.size()>0 && st.top()>=arr[i]){
+			while(st.size()>0 && st.top() >=arr[i]){
+				st.pop();
+			}
+		
+			if(st.size()==0){
+				ans.push_back(-1);
+			}
+			else{
+				ans.push_back(st.top());
+			}
+			}
+			st.push(arr[i]);
+		}
+		return ans;
+	}
+
+
+int main() { 
+
+
+vector<int> vect;
+vector<int> a;
+
+
+  
+    vect.push_back(10);
+    vect.push_back(20);
+    vect.push_back(30);
+    vect.push_back(84);
+    vect.push_back(3);
+    vect.push_back(12);
+    vect.push_back(98);
+
+    a = nextsmallertoleft(vect);
+        int n = a.size();
+        int m = vect.size();
+    
+     for(int i=0; i<m; i++){
+        cout << vect[i] << " ";
+    }
+    
+    cout<<" "<<endl;
+    
+    for(int i=0; i<n; i++){
+        cout << a[i] << " ";
+    }
+
+}
